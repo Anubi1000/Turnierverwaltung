6319fb1b7072ed19419b0b7ba45f7f70c269b5b1
Anubi1000 <72503115+Anubi1000@users.noreply.github.com>
2024-03-14 20:01:37 +0100

    Disable color transition in menu

--- frontend/src/app/dashboard/tournament/list/TournamentMenuButton.tsx
+++ frontend/src/app/dashboard/tournament/list/TournamentMenuButton.tsx
@@ -8,8 +8,8 @@ export function TournamentMenuButton() {
                 <span className="material-symbols-outlined">more_vert</span>
             </MenuButton>
         } menuClassName="z-50 bg-white p-2 rounded-md shadow-lg min-w-32 border -translate-x-1">
-            <MenuItem className="rounded-md px-3 py-1 hover:text-white hover:bg-black transition-colors select-none">Bearbeiten</MenuItem>
-            <MenuItem className="rounded-md px-3 py-1 hover:text-white hover:bg-black transition-colors select-none">Löschen</MenuItem>
+            <MenuItem className="rounded-md px-3 py-1 hover:text-white hover:bg-black select-none">Bearbeiten</MenuItem>
+            <MenuItem className="rounded-md px-3 py-1 hover:text-white hover:bg-black select-none">Löschen</MenuItem>
         </Menu>
     )
 }
