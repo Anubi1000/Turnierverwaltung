8113038592de098a664c033afae77b940f6ce953
Daniel Helmig <Helmig@fzi.de>
2024-03-16 14:11:19 +0100

    Formatting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,10 @@ import pino from "pino";
 import { closeDb, setupDB } from "./db";
 import { Tournament, TournamentInfo, TournamentValue } from "../../api/types";
 import { Collection, UUID } from "mongodb";
-import { v5 as uuid } from 'uuid';
+import { v5 as uuid } from "uuid";
 
 let env = process.env.NODE_ENV || "development";
-let UUID_NAMESPACE = 'tournaments';
+let UUID_NAMESPACE = "tournaments";
 let tournament_prefix = "tour_";
 
 let level;
@@ -51,24 +51,31 @@ async function main() {
     res.status(401).json({ errorMessage: "Password is invalid" });
   });
 
-  app.get("/tournaments", async (req, res) => { 
+  app.get("/tournaments", async (req, res) => {
     //returns array of tournament infos
     //tournament info is name and id
     var result: TournamentInfo[] = [];
-    const collection_info_cursor = await db.listCollections()
+    const collection_info_cursor = await db.listCollections();
     for await (const collection of collection_info_cursor) {
-      const info_doc = await db.collection(collection.name).findOne({type: "info"})
+      const info_doc = await db
+        .collection(collection.name)
+        .findOne({ type: "info" });
       if (info_doc != null) {
         const tournament_info: TournamentInfo = {
           name: info_doc.name,
           id: info_doc.id,
-        }
-        result.push(tournament_info)
-        logger.debug("Get Tournaments - Name: "+tournament_info.name +" and ID: "+tournament_info.id)
+        };
+        result.push(tournament_info);
+        logger.debug(
+          "Get Tournaments - Name: " +
+            tournament_info.name +
+            " and ID: " +
+            tournament_info.id,
+        );
       } else {
-        logger.warn(`Get Tournaments - No info doc for ${collection.name}`)
+        logger.warn(`Get Tournaments - No info doc for ${collection.name}`);
       }
-    }      
+    }
     res.status(200).send(result);
   });
 
@@ -78,19 +85,20 @@ async function main() {
     const name = tournament.name;
     const values = tournament.values;
     const id = uuid(name, UUID_NAMESPACE);
-    const info_doc = await db.collection(tournament_prefix+id.toString()).insertOne({     
-        "type": 'info',
-        "name": name,
-        "id": id,
-        "values": values  
-    });
+    const info_doc = await db
+      .collection(tournament_prefix + id.toString())
+      .insertOne({
+        type: "info",
+        name: name,
+        id: id,
+        values: values,
+      });
     if (info_doc == null) {
       res.status(400).send("The request contained invalid content");
-      logger.warn(`Create Tournament - No info doc created`)
+      logger.warn(`Create Tournament - No info doc created`);
     } else {
       res.status(201).send("The tournament has been created");
     }
-    
   });
 
   app.get("/tournaments/:tournamentId", async (req, res) => {
@@ -103,7 +111,9 @@ async function main() {
     const info_doc = await collection.findOne(info_query);
     var tournament_name, tournament_values;
     if (info_doc == null) {
-      res.status(404).send("The tournament with the specified id did not exist");
+      res
+        .status(404)
+        .send("The tournament with the specified id did not exist");
       return;
     } else {
       tournament_name = info_doc.name;
@@ -135,7 +145,6 @@ async function main() {
       info_doc.values = tournament.values;
       res.status(200).send("The tournament has been updated");
     }
-   
   });
 
   const server = app.listen(8080, () => {
